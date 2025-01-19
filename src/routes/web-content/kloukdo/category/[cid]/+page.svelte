d20a73a9588ed6f50415e8c34542b73c1df3346e
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2025-01-19 21:49:28 +0700

    #dev-continue-from-last-commit

---
+++ src/routes/web-content/kloukdo/category/[cid]/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from "svelte";
+  import { page } from '$app/stores';
+  import { getKLoukdoSubCategoriesByCategory } from "$providers/actions/kloukdo/kloukdosubcategory";
+
+    let categoryId = $page.params.cid;
+    
+    let SubCategories = [];
+
+    const loadSubCategories = async () => {
+        const res = await getKLoukdoSubCategoriesByCategory.load({category:categoryId});
+        SubCategories = res.data;
+    }
+
+    onMount(async () => {
+        await loadSubCategories();
+        console.log(slug)
+    })
+</script>
+
+<div>
+    {#each SubCategories as subCate}
+        <div>
+            {subCate.name}
+        </div>
+    {/each}
+</div>

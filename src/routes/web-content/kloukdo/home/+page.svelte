20779a98fac3afad890d75435e8e0116bb6be54a
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2025-01-26 00:15:53 +0700

    #dev-home-page

--- src/routes/web-content/kloukdo/home/+page.svelte
+++ src/routes/web-content/kloukdo/home/+page.svelte
@@ -1,18 +1,28 @@
 <script>
-  import ProductList from "$components/elements/channel/product-list/ProductList.svelte";
-  import ProductListItem from "$components/elements/channel/product-list/ProductListItem.svelte";
-  import ProductViewer from "$components/elements/product/view/ProductViewer.svelte";
+  import KLoukdoHeader from "$components/kloukdo/KLoukdoHeader.svelte";
   import KLoukdoProductListItem from "$components/kloukdo/KLoukdoProductListItem.svelte";
-import { getKLoukdoAdsImage } from "$providers/actions/kloukdo/kloukdoadsimage";
+  import { getKLoukdoAdsImage } from "$providers/actions/kloukdo/kloukdoadsimage";
   import { getKLoukdoCategories } from "$providers/actions/kloukdo/kloukdocategory";
-  import { getAllKLoukdoPromotios, getRandomKLoukdoPromotions } from "$providers/actions/kloukdo/kloukdopromotion";
+  import { getAllKLoukdoProducts } from "$providers/actions/kloukdo/kloukdoproduct";
+  import { getRandomKLoukdoPromotions } from "$providers/actions/kloukdo/kloukdopromotion";
   import { onMount } from "svelte";
+  import CorouselSlide from "$components/kloukdo/CorouselSlide.svelte";
+  import ProductDisplayOption from "$components/kloukdo/ProductDisplayOption.svelte";
+  import KLoukdoProductListView from "$components/kloukdo/KLoukdoProductListView.svelte";
+  import KLoukdoFooter from "$components/kloukdo/KLoukdoFooter.svelte";
 
     let Categories = [];
     let Ads = [];
     let Promotions = [];
+    let Products = [];
     let currentAds = 0;
+    let totalAds;
     const intervalTime = 5000;
+
+    let display = 2; //1. window display 2. list display
+
+    let currentPage = 0;
+    let currentLimit = 15;
     
     const loadCategories = async () => {
 		try {
@@ -38,93 +48,154 @@ import { getKLoukdoAdsImage } from "$providers/actions/kloukdo/kloukdoadsimage";
             
         }
     }
-
+    const loadProduct = async () => {
+        try {
+            const res = await getAllKLoukdoProducts.load({page:currentPage, limit:currentLimit});
+            Products = res.data;
+        } catch (error) {
+            
+        }
+    }
 
     setInterval(function () {
+        setTimeout(500)
         currentAds = (currentAds + 1) % Ads.length;
     }, intervalTime);
 
 	// When Operating start run it first
 	onMount(async () => {
-        // console.log("hi")
 		await loadCategories();
         await loadAds();
         await loadPromotion();
+        await loadProduct();
 
-        console.log(Promotions);
+        totalAds = Ads.length;
 	});
 </script>
 
-<div class="flex flex-col mx-auto px-5 w-full">
-    <div class=" text-xl font-bold m-auto">
-        K-Loukdo
-    </div>
-    <div class="m-auto mt-1 relative w-full">
-        {#each Ads as ad, i}
-            {#if i === currentAds}
+<div class="flex flex-col mx-auto w-96 border-blue-400 border overflow-y-scroll">
+    <KLoukdoHeader/>
+    
+    <div class="">
+        <div class="m-auto relative w-full">
+            {#each Ads as ad, i}
+                {#if i === currentAds}
+                    <div class=""
+                        >
+                        <img
+                            class="w-full h-44 bg-slate"
+                            src="{ad.image.url}"
+                            alt="{ad.name}" />
+                    </div>
+                {/if}
+            {/each}
+            <!-- <CorouselSlide
+                items={Ads};
+            /> -->
+
+            <div class="absolute bottom-[3px] right-2/4 translate-x-[50%]">
+                <div class="flex gap-2">
+                    
+                    {#each Array(totalAds) as _, index }
+                    
+                        <button class=""
+                            on:click={() => {
+                                currentAds = index
+                            }}    
+                        >
+                            <div class="rounded-full w-[8px] h-[8px] {currentAds === index ? 'bg-black' : 'bg-white'} border border-gray-400"></div>
+                        </button>
+                    {/each}
+                </div>
+
+            </div>
+    
+        </div>
+        <div >
+            <div class="grid grid-cols-4 gap-5 my-5">
+                {#each Categories as category }
+                    <div class="m-auto bg-slate-200 p-2 rounded-md">
+                        {#if category.icon}
+                            <a href="/web-content/kloukdo/category/{category.id}">
+                                <img src="{category.icon}" alt="" class="w-10 h-10">
+                            </a>
+                        {:else}
+                        <a href="/web-content/kloukdo/category/{category.id}">
+                            <img
+                            class=" w-10 h-10 m-auto"
+                            src="https://cdn.worldvectorlogo.com/logos/standout-stickers-1.svg"
+                            alt=""
+                        />
+                        </a>
+                        {/if}
+                    </div>
+                {/each}
+        
+            </div>
+            
+        </div>
+        <div class=" bg-white p-2">
+            <ProductDisplayOption
+                on:window={() => {display = 1}}
+                on:list={() => {display = 2}}
+            />
+
+            {#if display == 1}
                 <div class="">
-                    <img
-                        class="w-full h-44 rounded-lg bg-slate"
-                        src="{ad.image.url}"
-                        alt="{ad.name}" />
+                    <div class="flex gap-5">
+                        {#each Promotions as promo }
+                            <KLoukdoProductListItem
+                                product={promo.product}
+                                isAds={true}
+                                on:select={(evt) => {
+                                    let product = evt.detail;
+                                }}
+                            />
+                        {/each}
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <div class="grid gap-5 grid-cols-2">
+                        {#each Products as pro }
+                            <KLoukdoProductListItem
+                                product={pro}
+                                on:select={(evt) => {
+                                    let product = evt.detail;
+                                }}
+                            />
+                        {/each}
+                    </div>
+                </div>
+            {:else if display == 2}
+                <div class="">
+                    <div class="flex flex-col gap-5">
+                        {#each Promotions as promo }
+                            <KLoukdoProductListView
+                                product={promo.product}
+                                isAds={true}
+                                on:select={(evt) => {
+                                    let product = evt.detail;
+                                }}
+                            />
+                        {/each}
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <div class="flex flex-col gap-5">
+                        {#each Products as pro }
+                            <KLoukdoProductListView
+                                product={pro}
+                                on:select={(evt) => {
+                                    let product = evt.detail;
+                                }}
+                            />
+                        {/each}
+                    </div>
                 </div>
             {/if}
-        {/each}
-        <!-- <div class="absolute top-[5rem] left-3 bg-white flex justify-center border border-gray-600 p-1 rounded-md">
-            <button>
-                <svg
-                    class="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor">
-                    <path d="M8 12L14 6V18L8 12Z"></path>
-                </svg>
-            </button>
+            
         </div>
-        <div class="absolute top-[5rem] right-3">
-            <button>
-                <svg 
-                    class="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor">
-                    <path d="M16 12L10 18V6L16 12Z"></path>
-                </svg>
-            </button>
-        </div> -->
-
+        
     </div>
-    <div class="grid grid-cols-4 gap-1 mt-5">
-        {#each Categories as category }
-            <div class="m-auto bg-slate-200 p-2 rounded-md">
-                {#if category.icon}
-                    <a href="/web-content/kloukdo/category/{category.id}">
-                        <img src="{category.icon}" alt="" class="w-10 h-10">
-                    </a>
-                {:else}
-                <img
-                    class=" w-10 h-10 m-auto"
-                    src="https://cdn.worldvectorlogo.com/logos/standout-stickers-1.svg"
-                    alt=""
-                />
-                {/if}
-            </div>
-        {/each}
-
-    </div>
-
-    <div class="mt-5">
-        <p>Promotion</p>
-        <div class="flex gap-5">
-            {#each Promotions as promo }
-                <KLoukdoProductListItem
-                    product={promo.product}
-                    on:select={(evt) => {
-                        let product = evt.detail;
-                        console.log(product.product)
-                    }}
-                />
-            {/each}
-        </div>
-    </div>
+    <KLoukdoFooter/>
 </div>
